1c1f9b86a2ce8a5e430b133b135c449f628901af
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-09-22 11:59:00 +0300

    add PAtricipantQueue

---
+++ ConWork/src/main/java/org/example/model/ParticipantQueue.java
@@ -0,0 +1,38 @@
+package org.example.model;
+
+import java.util.Random;
+import java.util.PriorityQueue;
+
+
+/*
+  Класс, который представляет  собой очередь из участников.
+  Каждому участнику СЛУЧАЙНЫМ образом разыгрывается билет.
+ */
+public class ParticipantQueue  {
+
+    private int ticket;
+
+    private Random random;
+    private PriorityQueue<Participant> participants;
+
+    public ParticipantQueue() {
+        participants = new PriorityQueue<>();
+        random = new Random();
+    }
+
+    public void addParticipant(String name) {participants.add(new Participant(random.nextInt(100), name)); }
+
+    public boolean isEmpty() {
+        return participants.isEmpty();
+    }
+
+    public int getParticipants() {
+
+        return participants.peek() != null ? participants.peek().getTicket() : 0;
+    }
+
+    public void removeParticipants() {
+
+        participants.poll();
+    }
+}
